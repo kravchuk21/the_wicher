52a97ea7c9b390fcb08984cf6858182249ce3c83
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2020-06-24 16:58:23 +0300

    Day 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var mySwiper = new Swiper(".swiper-container", {
-  slidesPerView: 2,
+let mySwiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: ".arrow",
   },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+let menuBtn = document.querySelector(".menu-button");
+let header = document.querySelector(".header");
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("menu-button-active");
+  header.classList.toggle("header-active");
 });
